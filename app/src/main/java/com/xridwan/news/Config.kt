61156cb0753003886fa97c0b66147d1212594f5a
Ridwan <80747655+xridwan@users.noreply.github.com>
2025-06-07 23:27:56 +0700

    Update Config.kt

--- app/src/main/java/com/xridwan/news/Config.kt
+++ app/src/main/java/com/xridwan/news/Config.kt
@@ -2,7 +2,7 @@ package com.xridwan.news
 
 class Config {
     companion object {
-        const val API_KEY = "c2177d86f19a48eba585fe2e67612478"
+        const val API_KEY = ""
         const val BASE_URL = "https://newsapi.org/v2/"
     }
 }
